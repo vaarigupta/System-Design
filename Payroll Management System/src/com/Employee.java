8bb5eccb4ad90cfe8608be41aeb7b376b26cef72
Vaari Gupta <vaarigupta24@gmail.com>
2023-10-08 15:08:15 +0530

    Payroll management problem is added

---
+++ Payroll Management System/src/com/Employee.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Employee {
+
+    private int id;
+    private String name;
+    private double salary;
+
+    Employee(int id, String name, double salary)
+    {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+}
